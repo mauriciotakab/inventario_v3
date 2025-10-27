e2b19e5ef8e934a39f5764904decc90b8600138e
mauriciotakab <mauriciotakab@gmail.com>
2025-10-27 12:19:17 -0600

    Delete app directory

--- app/controllers/OrdenCompraController.php
+++
@@ -1,6 +0,0 @@
-private function checkAuth() {
-    if (empty($_SESSION['usuario_id'])) {
-        header('Location: /auth/index');
-        exit;
-    }
-}
--- app/views/productos/delete.php
+++
@@ -1,8 +0,0 @@
-<h2>Eliminar Producto</h2>
-<p>¿Estás seguro que deseas eliminar el producto <b><?= htmlspecialchars($producto['nombre']) ?></b> (ID: <?= $producto['id'] ?>)?<br>
-<b>Esta acción no se puede deshacer.</b></p>
-<form method="post">
-    <input type="hidden" name="confirmar" value="1">
-    <button type="submit">Sí, eliminar</button>
-    <a href="productos_view.php?id=<?= $producto['id'] ?>">Cancelar</a>
-</form>
